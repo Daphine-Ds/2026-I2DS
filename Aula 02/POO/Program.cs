b0c8b54f6bebb6462e57a6badb2f4d89d50d7840
Daphine-DS <sivieridaphine069@gmail.com>
2026-07-29 16:41:12 -0300

    Aula 02 - Classe e Objeto

--- Aula 02/POO/Program.cs
+++ Aula 02/POO/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Comentário de linha
+        /* 
+        Comentário 
+        de
+        varias 
+        linhas
+        */
+
+        //Escreve uma linha de texto na tela
+        Console.WriteLine("Programação Orienta a Objetos\n");
+        
+        Console.WriteLine("Abstração - 1º Pilar da POO");
+
+        /* Criar uma classe em C# 
+        A classe deveve ser declarada fora do bloco de programa*/
+
+        /*Criar um Objeto Apartir de uma classe*/
+
+        Pet pet1 = new Pet();
+
+        pet1.nome = "Totó";
+        pet1.tipo = "Cachorro";
+        pet1.idade = 3;
+
+        pet1.MostrarNome();
+
+        Pet pet2 = new Pet();
+
+        pet2.nome = "Mingau";
+        pet2.tipo = "Gato";
+        pet2.idade = 8;
+
+        pet2.MostrarNome();
+    }
+        public class Pet // Nome da ckasse smpre inicia com letras MAIUSCULAS
+    {
+        
+        // Declaração dos Atributos
+
+        public string nome;
+        public string tipo;
+        public int idade;
+
+        // Declaração dos Métodos
+
+        public void MostrarNome()
+        {
+            Console.WriteLine("Meu nome é " + this.nome);
+        }
+    } 
+}       
